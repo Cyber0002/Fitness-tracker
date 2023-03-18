859c279f6998666bc40833755d2b500bb327777d
Cyber0002 <123611208+Cyber0002@users.noreply.github.com>
2023-03-18 16:54:31 +0600

    Update Sets.cs

--- Fitness tracker/Entities/Sets.cs
+++ Fitness tracker/Entities/Sets.cs
@@ -6,6 +6,6 @@
         public int Set_number { get; set; }
         public int Reps { get; set; }
         public Exercises Exercise { get; set; }
-
+        public int ExerciseId { get; set; }
 }
     }
